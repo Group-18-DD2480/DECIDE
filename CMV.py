c98c6335469c5f1dd4570ce6e104ab270b80a40f
laykos0 <laykos0@protonmail.com>
2025-01-24 13:33:48 +0100

    [feat] Add LIC11

--- CMV.py
+++ CMV.py
@@ -9,3 +9,14 @@ def lic_10(X, Y, E_PTS, F_PTS, AREA1):
                 print((abs(X[a]*(Y[b] - Y[c]) + X[b]*(Y[c] - Y[a]) + X[c]*(Y[a] - Y[b])) / 2.0))
                 return True
     return False
+
+
+def lic_11(X, G_PTS):
+    if (n := len(X)) < 3:
+        return False
+    for i in range(n):
+        j = i + G_PTS + 1
+        if j < n and (X[j] - X[i]) < 0:
+            return True
+    return False
+    
