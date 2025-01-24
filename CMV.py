69560be02b9ac8786bacb99f860ef15363448ff9
laykos0 <laykos0@protonmail.com>
2025-01-24 18:21:29 +0100

    [feat] Add LIC12

--- CMV.py
+++ CMV.py
@@ -1,3 +1,6 @@
+import math
+
+
 def lic_10(X, Y, E_PTS, F_PTS, AREA1):
     if (n := len(X)) < 5:
         return False
@@ -19,4 +22,17 @@ def lic_11(X, G_PTS):
         if b < n and (X[b] - X[a]) < 0:
             return True
     return False
-    
+
+
+def lic_12(X, Y, K_PTS, LENGTH1, LENGTH2):
+    if (n := len(X)) < 3:
+        return False
+    g, l = False, False
+    for a in range(n):
+        if (b := a + K_PTS + 1) < n:
+            ab = math.sqrt(math.pow((X[b] - X[a]), 2) + math.pow((Y[b] - Y[a]), 2))
+            g = g if g else ab > LENGTH1
+            l = l if l else ab < LENGTH2
+            if g and l:
+                return True
+    return False    
