c8d5f6c2a04bfe771ced0cbb4e823d985859322b
laykos0 <laykos0@protonmail.com>
2025-01-24 13:33:48 +0100

    [feat] Add LIC11

--- CMV_test.py
+++ CMV_test.py
@@ -1,9 +1,9 @@
 import unittest
 
-from CMV import lic_10
+from CMV import lic_10, lic_11
+
 
 class TestLIC_10(unittest.TestCase):
-    
     def test_insufficient_points(self):
         X, Y = [0, 1, 2], [0, 1, 0]
         E_PTS, F_PTS, AREA1 = 1, 1, 1
@@ -18,6 +18,20 @@ class TestLIC_10(unittest.TestCase):
         X, Y = [0, 1, 2, 3, 4], [0, 1, 1, 1, 0]
         E_PTS, F_PTS, AREA1 = 1, 1, 2
         self.assertFalse(lic_10(X, Y, E_PTS, F_PTS, AREA1))
+
+
+class TestLIC_11(unittest.TestCase):
+    def test_insufficient_points(self):
+        X, G_PTS = [0, 1], 1
+        self.assertFalse(lic_11(X, G_PTS))
+        
+    def test_difference_positive(self):
+        X, G_PTS = [0, 1, 2, 3, 4, 5], 1
+        self.assertFalse(lic_11(X, G_PTS))
+    
+    def test_difference_negative(self):
+        X, G_PTS = [5, 4, 3, 2, 1, 0], 1
+        self.assertTrue(lic_11(X, G_PTS))
 
 
 if __name__ == '__main__':
