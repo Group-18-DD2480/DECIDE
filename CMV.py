37e06d952a6f14db260210b2440101ca205b72e6
laykos0 <laykos0@protonmail.com>
2025-01-24 13:16:21 +0100

    [feat] Add LIC10

---
+++ CMV.py
@@ -0,0 +1,11 @@
+def lic_10(X, Y, E_PTS, F_PTS, AREA1):
+    if (n := len(X)) < 5:
+        return False
+    for a in range(n):
+        b = a + E_PTS + 1
+        c = b + F_PTS + 1
+        if c < n:
+            if (abs(X[a]*(Y[b] - Y[c]) + X[b]*(Y[c] - Y[a]) + X[c]*(Y[a] - Y[b])) / 2.0) > AREA1:
+                print((abs(X[a]*(Y[b] - Y[c]) + X[b]*(Y[c] - Y[a]) + X[c]*(Y[a] - Y[b])) / 2.0))
+                return True
+    return False
---
+++ CMV_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from CMV import lic_10
+
+class TestLIC_10(unittest.TestCase):
+    
+    def test_insufficient_points(self):
+        X, Y = [0, 1, 2], [0, 1, 0]
+        E_PTS, F_PTS, AREA1 = 1, 1, 1
+        self.assertFalse(lic_10(X, Y, E_PTS, F_PTS, AREA1))
+        
+    def test_area_greater(self):
+        X, Y = [0, 1, 2, 3, 4], [0, 1, 2, 1, 5]
+        E_PTS, F_PTS, AREA1 = 1, 1, 0.5
+        self.assertTrue(lic_10(X, Y, E_PTS, F_PTS, AREA1))
+        
+    def test_area_smaller(self):
+        X, Y = [0, 1, 2, 3, 4], [0, 1, 1, 1, 0]
+        E_PTS, F_PTS, AREA1 = 1, 1, 2
+        self.assertFalse(lic_10(X, Y, E_PTS, F_PTS, AREA1))
+
+
+if __name__ == '__main__':
+    unittest.main()
