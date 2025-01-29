c5518396c3eeb0c1fe3f500100f982a8ddc4d9e2
YusufDemir1210 <yusufdemir12610@gmail.com>
2025-01-29 21:46:32 +0100

    feat: implemented PUM, related to issue #33 and its subissue #35

---
+++ pum_fuv.py
@@ -0,0 +1,8 @@
+def construct_PUM(LCM, CMV, LENGTH = 15):
+    PUM = []
+    for i in range(LENGTH):
+        tempRow = []
+        for j in range(LENGTH):
+            tempRow.append(LCM[i][j](CMV[i], CMV[j]))
+        PUM.append(tempRow)
+    return PUM
