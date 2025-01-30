da4f3a6797f68cfbd2644b46269275abd1f7ad06
YusufDemir1210 <yusufdemir12610@gmail.com>
2025-01-30 12:39:15 +0100

    [refactor] deleted unused file (#35, #38)

--- pum_fuv.py
+++
@@ -1,8 +0,0 @@
-def construct_PUM(LCM, CMV, LENGTH = 15):
-    PUM = []
-    for i in range(LENGTH):
-        tempRow = []
-        for j in range(LENGTH):
-            tempRow.append(LCM[i][j](CMV[i], CMV[j]))
-        PUM.append(tempRow)
-    return PUM
