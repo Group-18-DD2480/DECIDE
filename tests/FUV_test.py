671a0b3bbbf46fe0e62fed5ed40f628f684778ac
RuriThomas <116952548+RuriThomas@users.noreply.github.com>
2025-01-30 15:13:16 +0100

    [refactor] Changed name of file to be consisent with other test files and import the FUV function (#37)

--- tests/FUV_test.py
+++ tests/FUV_test.py
@@ -1,39 +1,37 @@
 import unittest
-
+from src.FUV import FUV as function
 
 rows, cols = 15, 15  
-def FUV(PUM, PUV):
-    return [False for _ in range(rows)]
 class TestFUV(unittest.TestCase):
     
-    def PUV_all_false(self):
+    def test_PUV_all_false(self):
         PUM = [[False] * cols for _ in range(rows)]
         PUV = [False for _ in range(rows)]
-        FUV = FUV(PUM, PUV)
+        FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertTrue(FUV[i])
-    def PUV_and_PUM_all_true(self):
+    def test_PUV_and_PUM_all_true(self):
         PUM = [[True] * cols for _ in range(rows)]
         PUV = [True for _ in range(rows)]
-        FUV = FUV(PUM, PUV)
+        FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertTrue(FUV[i])
     
-    def PUM_all_false(self):
+    def test_PUM_all_false(self):
         PUM = [[False] * cols for _ in range(rows)]
         PUV = [True for _ in range(rows)]
-        FUV = FUV(PUM, PUV)
+        FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertFalse(FUV[i])
-    def PUM_one_false_in_every_row(self):
+    def test_PUM_one_false_in_every_row(self):
         PUM = [[True] * cols for _ in range(rows)]
         PUV = [True for _ in range(rows)]
         for i in range(rows):
             PUM[i][i] = False
-        FUV = FUV(PUM, PUV)
+        FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertFalse(FUV[i])
-    def general_PUM_and_PUV(self):
+    def test_general_PUM_and_PUV(self):
         PUM = [
             [False, True, True, True, True, True, True, True, False, False, True, True, True, True, True],
             [True, True, True, True, True, True, True, True, True, False, False, True, True, True, True],
@@ -56,7 +54,7 @@ class TestFUV(unittest.TestCase):
         PUV = [False, True, False, True, True, 
                True, True, True, False, False, 
                True, True, False, True, True]
-        FUV_func = FUV(PUM,PUV)
+        FUV_func = function(PUM,PUV)
         FUV_test = [True, False, True, False, False, 
                     True, False, False, True, True,
                     False, False, True, True, True]
