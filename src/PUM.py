38accf7793e1d9aaebd7ef8e698f9ab19ef3ec46
AlessandroColi <acoli@kth.se>
2025-01-30 14:21:07 +0100

    [feat] checks in PUM (#59)
    added input validations checks in PUM

--- src/PUM.py
+++ src/PUM.py
@@ -1,8 +1,27 @@
-def PUM(LCM, CMV, LENGTH = 15):
+from src.decide import InvalidInputException
+
+def PUM(LCM, CMV):
+    
+    if not isinstance(LCM, list) or not isinstance(CMV, list):
+        raise InvalidInputException
+    
+    LENGTH = len(CMV)
+    
+    if len(LCM) != LENGTH or any(len(row) != LENGTH for row in LCM):
+        raise InvalidInputException
+    
     PUM = []
     for i in range(LENGTH):
         tempRow = []
+        
         for j in range(LENGTH):
-            tempRow.append(LCM[i][j](CMV[i], CMV[j]))
+            
+            if i == j :
+                tempRow.append(None)
+            elif LCM[i][j] == LCM[j][i] :
+                tempRow.append(LCM[i][j](CMV[i], CMV[j]))
+            else:
+                raise InvalidInputException
+            
         PUM.append(tempRow)
     return PUM
