9cfe60f6b8f3e163c956554f7cb34ae7f5702b04
YusufDemir1210 <yusufdemir12610@gmail.com>
2025-01-30 22:50:01 +0100

    [feat] added unit tests for LAUNCH function (#62)
    added one unit tests for each of these cases:
    positive, negative, invalid_input

---
+++ tests/LAUNCH_test.py
@@ -0,0 +1,20 @@
+import unittest
+from src.LAUNCH import LAUNCH
+from src.decide import InvalidInputException
+
+class Test_LAUNCH(unittest.TestCase):
+
+    def test_LAUNCH_positive(self):
+        # FUV is a correct sized vector and all entries are True
+        FUV = [True for _ in range(15)]
+        self.assertTrue(LAUNCH(FUV))
+
+    def test_LAUNCH_negative(self):
+        # FUV is a correct sized vector but at least one entry is False
+        FUV = [False for _ in range(15)]
+        self.assertFalse(LAUNCH(FUV))
+
+    def test_LAUNCH_invalid(self):
+        # size of FUV is not correct
+        FUV = [True, True]
+        self.assertRaises(InvalidInputException)
