c61882bb2825d0c8fa7f3a4eff69a0ddd2328168
RuriThomas <116952548+RuriThomas@users.noreply.github.com>
2025-01-31 12:35:49 +0100

    [feat] Added the input validation checks for FUV as well as the corresponding tests and modified the existing test to have valid inputs (#67)

--- src/FUV.py
+++ src/FUV.py
@@ -1,4 +1,6 @@
-def FUV(PUM, PUV, LENGTH = 15):
+from src.decide import InvalidInputException
+
+def FUV(PUM, PUV):
     """
     Constructs the Final Unlocking Vector (FUV) based on the Preliminary Unlocking Matrix (PUM) and the Preliminary Unlocking Vector (CMV).
 
@@ -11,13 +13,33 @@ def FUV(PUM, PUV, LENGTH = 15):
         if all PUM[i][j] are True disregarding PUM[i][i], otherwise it is False.
     
     Raises:
-        InvalidInputException: If PUM or PUV is not a list, if their sizes do not match, or if PUM 
-            is not symmetric.
+        InvalidInputException: If PUM or PUV is not a list, if their sizes do not match, if PUM 
+            is not symmetric or if PUM doesn't have None one the diagonal.
     """
+    if not isinstance(PUM, list) or not isinstance(PUV, list):
+        raise InvalidInputException
+    
+    LENGTH = len(PUV)
+    
+    if len(PUM) != LENGTH or any(len(row) != LENGTH for row in PUM):
+        raise InvalidInputException
+    
+    for i in range(LENGTH):
+        for j in range(LENGTH):
+            if i == j and not PUM[i][j] == None:
+                raise InvalidInputException
+            if i != j and not PUM[i][j] == PUM[j][i]:
+                raise InvalidInputException
+
+
     FUV = []
     for i in range(LENGTH):
         if not PUV[i]: # FUV[i] is true if PUV[i] is false
            FUV.append(True)
-        else: # or all elements of PUM[i] is true
-            FUV.append(all(PUM[i]))
+        else: # or all elements of PUM[i] are true except PUM[i][i]
+            unlock = True
+            for j in range(LENGTH):
+                if unlock and i != j and (not PUM[i][j]):
+                    unlock = False
+            FUV.append(unlock)
     return FUV
--- tests/FUV_test.py
+++ tests/FUV_test.py
@@ -1,17 +1,54 @@
 import unittest
 from src.FUV import FUV as function
+from src.decide import InvalidInputException
+
 
 rows, cols = 15, 15  
 class TestFUV(unittest.TestCase):
     """Test suite for Final Unlocking Vector.
 
-    Condition: For FUV[i] to be true, either PUV[i] is False or for all j, PUM[i][j] must be true.
+    Condition: For FUV[i] to be true, either PUV[i] is False or for all j, PUM[i][j] must be true except for PUM[i][i].
     """
-    
+    def test_invalid_PUV_not_list(self):
+        """Test that FUV fails when PUM isn't a list"""
+        PUM = [[False] * cols for _ in range(rows)]
+        for i in range(rows):
+            PUM[i][i] = None
+        PUV = 3
+        self.assertRaises(InvalidInputException,function,PUM,PUV)
+    def test_invalid_PUM_not_list(self):
+        """Test that FUV fails when PUV isn't a list"""
+        PUM = 1
+        PUV = [True for _ in range(rows)]
+        self.assertRaises(InvalidInputException,function,PUM,PUV)
+    def test_invalid_input_sizes(self):
+        """Test that FUV fails when PUM and PUM's sizes don't match"""
+        PUM = [[False] * cols for _ in range(rows)]
+        for i in range(rows):
+            PUM[i][i] = None
+        PUV = [True for _ in range(rows+1)]
+        self.assertRaises(InvalidInputException,function,PUM,PUV)
+    def test_invalid_PUM_symetric(self):
+        """Test that FUV fails when PUM isn't a symetric"""
+        PUM = [[False] * cols for _ in range(rows)]
+        for i in range(rows):
+            PUM[i][i] = None
+        PUV = [True for _ in range(rows)]
+        PUM[0][1] = True
+        self.assertRaises(InvalidInputException,function,PUM,PUV)
+    def test_invalid_PUM_None(self):
+        """Test that FUV fails when PUM doesn't have None on the diagonal"""
+        PUM = [[False] * cols for _ in range(rows)]
+        PUV = [True for _ in range(rows)]
+        self.assertRaises(InvalidInputException,function,PUM,PUV)
+
+
     def test_PUV_all_false(self):
         """Test when every element of PUV is False, that all elements of FUV are True"""
         PUM = [[False] * cols for _ in range(rows)]
         PUV = [False for _ in range(rows)]
+        for i in range(rows):
+            PUM[i][i] = None
         FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertTrue(FUV[i])
@@ -19,6 +56,8 @@ class TestFUV(unittest.TestCase):
         """Test when all the PUM is True and the PUV is all True (to make the PUM matter), that we get a completly True FUV"""
         PUM = [[True] * cols for _ in range(rows)]
         PUV = [True for _ in range(rows)]
+        for i in range(rows):
+            PUM[i][i] = None
         FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertTrue(FUV[i])
@@ -27,16 +66,36 @@ class TestFUV(unittest.TestCase):
         """Test when all the PUM is False and the PUV is all True (to make the PUM matter), that all elements of FUV are False"""
         PUM = [[False] * cols for _ in range(rows)]
         PUV = [True for _ in range(rows)]
+        for i in range(rows):
+            PUM[i][i] = None
         FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertFalse(FUV[i])
     def test_PUM_one_false_in_every_row(self):
-        """Test when exactly one element per row of PUM is False and all the PUV is True (to make the PUM matter),
+        """Test when at least one element per row of PUM is False and all the PUV is True (to make the PUM matter),
         that all the FUV is False"""
-        PUM = [[True] * cols for _ in range(rows)]
+        PUM = [
+            [True, False, True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [False, True, True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, False, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, False, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, False, True, True, True, True, True, True, True, True, True],
+            
+            [True, True, True, True, False, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, False, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, False, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, False, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, False, True, True, True, True, True, True],
+
+            [True, True, True, True, True, True, True, True, True, True, True, False, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, False, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True, False, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, False, True, False],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True, False, True],
+        ]
         PUV = [True for _ in range(rows)]
         for i in range(rows):
-            PUM[i][i] = False
+            PUM[i][i] = None
         FUV = function(PUM, PUV)
         for i in range(rows):
             self.assertFalse(FUV[i])
@@ -44,29 +103,29 @@ class TestFUV(unittest.TestCase):
     def test_general_PUM_and_PUV(self):
         """Test a general non specific example"""
         PUM = [
-            [False, True, True, True, True, True, True, True, False, False, True, True, True, True, True],
-            [True, True, True, True, True, True, True, True, True, False, False, True, True, True, True],
-            [True, True, True, True, True, True, True, True, False, False, False, True, True, True, True],
-            [True, True, True, True, True, True, True, True, False, True, False, True, True, True, False],
-            [True, True, True, True, False, True, True, True, False, False, False, True, True, True, True],
+            [None, False, False, True, True, True, True, False, True, True, True, True, True, True, True],
+            [False, None, True, False, True, True, True, True, True, True, True, True, True, True, True],
+            [False, True, None, False, True, True, True, True, True, True, True, True, True, True, True],
+            [True, False, False, None, False, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, False, None, True, True, True, True, True, True, True, True, True, True],
             
-            [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True],
-            [True, False, True, True, True, True, True, True, False, False, False, True, True, True, True],
-            [True, True, True, True, True, True, True, True, True, False, False, True, True, False, True],
-            [True, True, True, True, True, True, True, True, False, False, False, True, True, True, True],
-            [True, True, True, True, True, True, True, True, False, True, False, True, True, True, True],
-            
-            [True, True, True, True, True, True, True, True, False, False, False, True, True, True, True],
-            [True, True, True, True, True, False, True, True, False, False, False, True, True, True, True],
-            [True, True, True, True, True, True, True, True, False, False, False, True, True, True, True],
-            [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, None, True, True, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, None, True, True, True, True, True, True, True, True],
+            [False, True, True, True, True, True, True, None, True, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, None, True, True, True, True, True, True],
+            [True, True, True, True, True, True, True, True, True, None, True, True, True, True, True],
+
+            [True, True, True, True, True, True, True, True, True, True, None, False, True, True, True],
+            [True, True, True, True, True, True, True, True, True, True, False, None, True, True, False],
+            [True, True, True, True, True, True, True, True, True, True, True, True, None, True, True],
+            [True, True, True, True, True, True, True, True, True, True, True, True, True, None, True],
+            [True, True, True, True, True, True, True, True, True, True, True, False, True, True, None],
         ]
         PUV = [False, True, False, True, True, 
                True, True, True, False, False, 
                True, True, False, True, True]
         FUV_func = function(PUM,PUV)
         FUV_test = [True, False, True, False, False, 
-                    True, False, False, True, True,
-                    False, False, True, True, True]
+                    True, True, False, True, True,
+                    False, False, True, True, False]
         self.assertEqual(FUV_func, FUV_test)
