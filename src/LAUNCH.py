b3511993071314286db080060c531f4e09a23811
Yusuf Demir <139581075+YusufDemir1210@users.noreply.github.com>
2025-01-31 09:21:20 +0100

    [feat] LAUNCH implementation and tests (#68)
    * [feat] implementation of LAUNCH function (#62)
    
    Implemented LAUNCH function, including error conditions.
    
    * [feat] added unit tests for LAUNCH function (#62)
    
    added one unit tests for each of these cases:
    positive, negative, invalid_input
    
    * [fix] fixed invalid_input unit test (#62)

---
+++ src/LAUNCH.py
@@ -0,0 +1,20 @@
+from src.decide import InvalidInputException
+
+def LAUNCH(FUV):
+    """
+    Constructs the LAUNCH boolean variable based on FUV (Final Unlocking Vector)
+
+    Args:
+        FUV (list of bool): Final Unlocking Vector.
+
+    Returns:
+        boolean value
+
+    Raises:
+        InvalidInputException: If FUV is not a list, if its size does not match
+    """
+
+    if type(FUV) != list or len(FUV) != 15:
+        raise InvalidInputException
+
+    return all(FUV)
